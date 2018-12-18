6780f65780d1edb2dff8f15305b38ba0c3617e17
Jordan Goldstein <JordanGoldstein@gatech.edu>
2018-12-18 18:19:02 +0800

    First run ~38%

---
+++ first.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+import pandas as pd
+import tensorflow as ts
+from keras import backend as K
+from keras.layers import (Activation, Conv2D, Dense, Dropout, Flatten,
+                          MaxPooling2D)
+from keras.losses import categorical_crossentropy
+from keras.models import Sequential
+from keras.optimizers import Adadelta
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+image_path = "./smallimages/"
+save_path = "./smallimages/"
+print("Loading Mapping info")
+map = pd.read_csv("train.csv")
+ids = map["Id"]
+image_names = map["Image"]
+unique_ids = set(ids)
+num_ids = len(unique_ids)
+print("Mapping Info Loaded")
+print("Loading Images")
+images = []
+count = 1
+for image in image_names:
+    img = cv2.resize(cv2.imread(image_path + image),(28,28))
+    print(count)
+    count +=1
+    images.append(img)
+    #cv2.imwrite(save_path + image, img)
+
+images = np.array(images)
+print("Images Loaded")
+print("Building Train and Test Sets")
+ids = np.array(ids)
+print(images.shape)
+input_shape = (28,28,3)
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(ids)
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+labels = onehot_encoder.fit_transform(integer_encoded)
+train_x, test_x, train_y, test_y = train_test_split(images, labels, test_size=.3)
+print(train_x.shape)
+print(test_x.shape)
+print(train_y.shape)
+print(test_y.shape)
+print("Train and Tests sets built")
+print("Building Model")
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_ids, activation='softmax'))
+
+print("Model Built")
+print("Compiling Model")
+model.compile(loss=categorical_crossentropy,
+              optimizer=Adadelta(),
+              metrics=['accuracy'])
+print("Model Compiled")
+print("Training Model")
+batch_size = 1
+num_epoch = 1
+model_log = model.fit(train_x, train_y,
+          batch_size=batch_size,
+          epochs=num_epoch,
+          verbose=1,
+          validation_data=(test_x, test_y))
+print("Model Trained")
+print("Testing Model")
+score = model.evaluate(test_x, test_y, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
